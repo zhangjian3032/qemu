8098de719a516d47e234cd8c99412c0d45b392b1
Cédric Le Goater <clg@kaod.org>
2019-04-02 22:49:35 +0200

    aspeed: add a GPIO controller to the SoC
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- include/hw/arm/aspeed_soc.h
+++ include/hw/arm/aspeed_soc.h
@@ -23,6 +23,7 @@
 #include "hw/watchdog/wdt_aspeed.h"
 #include "hw/net/ftgmac100.h"
 #include "hw/misc/aspeed_ibt.h"
+#include "hw/gpio/aspeed_gpio.h"
 
 #define ASPEED_SPIS_NUM  2
 #define ASPEED_WDTS_NUM  3
@@ -45,6 +46,7 @@ typedef struct AspeedSoCState {
     AspeedWDTState wdt[ASPEED_WDTS_NUM];
     FTGMAC100State ftgmac100;
     AspeedIBTState ibt;
+    AspeedGPIOState gpio;
 } AspeedSoCState;
 
 #define TYPE_ASPEED_SOC "aspeed-soc"
