8d05101dca232827ec248e999db0fd3cde761851
Cédric Le Goater <clg@kaod.org>
2020-12-09 09:35:52 +0100

    i2c: Add a IBM CFF Power Supply device
    Simple model of a PSU device of the witherspoon.
    
    TODO:
       - generate "real" values
       - implement property visitors to generate faults.
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- hw/arm/aspeed.c
+++ hw/arm/aspeed.c
@@ -582,6 +582,12 @@ static void witherspoon_bmc_i2c_init(AspeedMachineState *bmc)
         qdev_connect_gpio_out(dev, pca1_leds[i].gpio_id,
                               qdev_get_gpio_in(DEVICE(led), 0));
     }
+
+    i2c_slave_create_simple(aspeed_i2c_get_bus(&soc->i2c, 3), "ibm-cffps",
+        0x68);
+    i2c_slave_create_simple(aspeed_i2c_get_bus(&soc->i2c, 3), "ibm-cffps",
+        0x69);
+
     i2c_slave_create_simple(aspeed_i2c_get_bus(&soc->i2c, 4), "tmp423", 0x4c);
     i2c_slave_create_simple(aspeed_i2c_get_bus(&soc->i2c, 5), "tmp423", 0x4c);
 
