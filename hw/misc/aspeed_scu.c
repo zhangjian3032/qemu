8a4968623a3a9d9cc0d1147ac25fcbab8b044e0e
Andrew Jeffery <andrew@aj.id.au>
2020-12-09 09:35:52 +0100

    hw/adc: Add basic Aspeed ADC model
    This model implements enough behaviour to do basic functionality tests
    such as device initialisation and read out of dummy sample values. The
    sample value generation strategy is similar to the STM ADC already in
    the tree.
    
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- hw/misc/aspeed_scu.c
+++ hw/misc/aspeed_scu.c
@@ -293,7 +293,6 @@ static void aspeed_ast2500_scu_write(void *opaque, hwaddr offset,
     if (reg > PROT_KEY && reg < CPU2_BASE_SEG1 &&
             !s->regs[PROT_KEY]) {
         qemu_log_mask(LOG_GUEST_ERROR, "%s: SCU is locked!\n", __func__);
-        return;
     }
 
     trace_aspeed_scu_write(offset, size, data);
